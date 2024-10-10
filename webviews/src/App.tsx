5dd21f48d4aad79cb9c4003aed01725a9de4d78c
sachinsuresh-s10 <sachinsuresh.s10@ibm.com>
2024-10-10 13:39:32 +0530

    Revert "Merge pull request #2 from SachinS10-lab/add-option-existing-models"
    This reverts commit 5811d8188558b877aaad6cefbb5d6e772671e2ae, reversing
    changes made to 0d36650879f91aeac057a69ac127bfa9406991cc.

--- webviews/src/App.tsx
+++ webviews/src/App.tsx
@@ -11,12 +11,10 @@ function App() {
     { label: 'granite-code:3b', value: 'granite-code:3b', info: '2.0 GB' },
     { label: 'granite-code:8b', value: 'granite-code:8b', info: '4.6 GB' },
     { label: 'granite-code:20b', value: 'granite-code:20b', info: '12 GB' },
-    { label: 'granite-code:34b', value: 'granite-code:34b', info: '19 GB' },
-    { label: 'Keep existing configuration', value: 'Keep existing configuration', info: '' }
+    { label: 'granite-code:34b', value: 'granite-code:34b', info: '19 GB' }
   ];
   const embeddingsOptions: ModelOption[] = [
-    { label: 'nomic-embed-text', value: 'nomic-embed-text', info: '274 MB' },
-    { label: 'Keep existing configuration', value: 'Keep existing configuration', info: '' }
+    { label: 'nomic-embed-text', value: 'nomic-embed-text', info: '274 MB' }
   ];
   const [tabModel, setTabModel] = useState<string | null>(modelOptions[1].value); //use 8b by default
   const [chatModel, setChatModel] = useState<string | null>(modelOptions[1].value);//use 8b by default
@@ -181,13 +179,8 @@ function App() {
           className="model-list"
           label="Chat model"
           value={chatModel}
-          onChange={(e) => 
-            { e?.value !== "Keep existing configuration" ? 
-              (setChatModel(e?.value ?? null)) : 
-              (setChatModel(null));
-            }
-          }
-          status={chatModel === null ? (null) : (isModelAvailable(chatModel))}
+          onChange={(e) => setChatModel(e?.value ?? null)}
+          status={isModelAvailable(chatModel)}
           options={modelOptions}
           progress={chatModel ? modelPullProgress[chatModel] : undefined}
           disabled={!enabled}
@@ -197,13 +190,8 @@ function App() {
           className="model-list"
           label="Tab completion model"
           value={tabModel}
-          onChange={(e) =>
-            { e?.value !== "Keep existing configuration" ? 
-              (setTabModel(e?.value ?? null)) :
-              (setTabModel(null));
-            }
-          }
-          status={tabModel === null ? (null) : (isModelAvailable(tabModel))}
+          onChange={(e) => setTabModel(e?.value ?? null)}
+          status={isModelAvailable(tabModel)}
           options={modelOptions}
           progress={tabModel ? modelPullProgress[tabModel] : undefined}
           disabled={!enabled}
@@ -213,13 +201,8 @@ function App() {
           className="model-list"
           label="Embeddings model"
           value={embeddingsModel}
-          onChange={(e) => 
-            { e?.value !== "Keep existing configuration" ? 
-              (setEmbeddingsModel(e?.value ?? null)) :
-              (setEmbeddingsModel(null));
-            }
-          }
-          status={embeddingsModel === null ? (null) : (isModelAvailable(embeddingsModel))}
+          onChange={(e) => setEmbeddingsModel(e?.value ?? null)}
+          status={isModelAvailable(embeddingsModel)}
           options={embeddingsOptions}
           progress={embeddingsModel ? modelPullProgress[embeddingsModel] : undefined}
           disabled={!enabled}
